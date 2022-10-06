ece1d5b016082795169160dd2e035762fd0e8caa
AgoraIO <ivanovsin11@gmail.com>
2022-10-06 11:27:29 -0700

    -

--- codebattle/settings.py
+++ codebattle/settings.py
@@ -11,24 +11,29 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 """
 
+from doctest import testmod
 import os
 
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+from dotenv import load_dotenv
+load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3l*_pm9b&etcz$l$am+o$ijt6$()++w*285rgdc=g$jjf%8p4%'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get('DEBUG')) == "1"
 
-ALLOWED_HOSTS = ['https://codebattle.up.railway.app', 'codebattle.up.railway.app', '127.0.0.1']
+print('DEBUG:', DEBUG)
+
+ALLOWED_HOSTS = ['https://codebattle.up.railway.app', 'codebattle.up.railway.app', '127.0.0.1', 'localhost']
 
 AUTH_USER_MODEL = 'base.User'
 # Application definition
@@ -89,7 +94,6 @@ WSGI_APPLICATION = 'codebattle.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
 
 DATABASES = {
     'default': {
@@ -159,3 +163,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CSRF_TRUSTED_ORIGINS = [
     'https://codebattle.up.railway.app',
 ]
+
+#DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_QUERYSTRING_AUTH = False
+
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
